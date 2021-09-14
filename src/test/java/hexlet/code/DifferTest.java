ccc1a78bcc5f6018b42c6aa5ad548b7843ac588d
igor.onuferko <igor.nfrk@gmail.com>
2021-09-14 02:41:58 +0200

    Add tests checkstyle

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -8,21 +8,21 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 class DifferTest {
-    Path pathTestFile1 = Path.of("src/test/java/hexlet/code/TestFile1.json");
-    Path pathTestFile2 = Path.of("src/test/java/hexlet/code/TestFile2.json");
-    Path pathTestFileIsEmpty = Path.of("src/test/java/hexlet/code/TestFile3.json");
-    Path pathNonValid = Path.of("non-valid-filepath");
+    private final Path pathTestFile1 = Path.of("src/test/java/hexlet/code/TestFile1.json");
+    private final Path pathTestFile2 = Path.of("src/test/java/hexlet/code/TestFile2.json");
+    private final Path pathTestFileIsEmpty = Path.of("src/test/java/hexlet/code/TestFile3.json");
+    private final Path pathNonValid = Path.of("non-valid-filepath");
 
     @Test
     void testDiffer1() throws DifferExceptions, IOException {
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "    host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
-                "  + timeout: 20\n" +
-                "  + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
         assertThat(Differ.generate(pathTestFile1, pathTestFile2)).isEqualTo(expected);
     }
 
