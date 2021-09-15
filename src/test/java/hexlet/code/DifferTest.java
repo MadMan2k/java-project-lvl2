a6e065e4025f676979439e5d7e57c3ad9f742380
igor.onuferko <igor.nfrk@gmail.com>
2021-09-15 12:47:34 +0200

    DifferTest optimization2

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -9,41 +9,13 @@ import java.nio.file.Path;
 
 class DifferTest {
     private final Path pathTestFile1 = Path.of("src/test/resources/TestFile1.json");
-    private final Path pathTestFile2 = Path.of("src/test/resources/TestFile2.json");
-    private final Path pathTestFile3 = Path.of("src/test/resources/TestFile3.yml");
-    private final Path pathTestFile4 = Path.of("src/test/resources/TestFile4.yml");
+    private final Path pathTestFile2 = Path.of("src/test/resources/TestFile2.yml");
     private final Path pathTestFileIsEmpty = Path.of("src/test/resources/TestFileEmpty.json");
     private final Path pathNonValid = Path.of("non-valid-filepath");
     private final Path pathTestFormatNotSupported = Path.of("src/test/resources/TestFormatNotSupported.txt");
 
-//    @Test
-//    void testDifferJson() throws DifferExceptions, IOException {
-//        String expected = "{\n"
-//                + "  - follow: false\n"
-//                + "    host: hexlet.io\n"
-//                + "  - proxy: 123.234.53.22\n"
-//                + "  - timeout: 50\n"
-//                + "  + timeout: 20\n"
-//                + "  + verbose: true\n"
-//                + "}";
-//        assertThat(Differ.generate(pathTestFile1, pathTestFile2)).isEqualTo(expected);
-//    }
-//
-//    @Test
-//    void testDifferYaml() throws DifferExceptions, IOException {
-//        String expected = "{\n"
-//                + "  - follow: false\n"
-//                + "    host: hexlet.io\n"
-//                + "  - proxy: 123.234.53.22\n"
-//                + "  - timeout: 50\n"
-//                + "  + timeout: 20\n"
-//                + "  + verbose: true\n"
-//                + "}";
-//        assertThat(Differ.generate(pathTestFile3, pathTestFile4)).isEqualTo(expected);
-//    }
-
     @Test
-    void testDifferJsonYamlMix() throws DifferExceptions, IOException {
+    void testDiffer() throws DifferExceptions, IOException {
         String expected = "{\n"
                 + "  - follow: false\n"
                 + "    host: hexlet.io\n"
@@ -52,7 +24,7 @@ class DifferTest {
                 + "  + timeout: 20\n"
                 + "  + verbose: true\n"
                 + "}";
-        assertThat(Differ.generate(pathTestFile1, pathTestFile4)).isEqualTo(expected);
+        assertThat(Differ.generate(pathTestFile1, pathTestFile2)).isEqualTo(expected);
     }
 
     @Test
