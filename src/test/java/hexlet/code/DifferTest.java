f3f47fc74ee8302bfa4510673ecab0567a1e397d
igor.onuferko <igor.nfrk@gmail.com>
2021-09-15 12:42:47 +0200

    DifferTest optimization

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -16,31 +16,31 @@ class DifferTest {
     private final Path pathNonValid = Path.of("non-valid-filepath");
     private final Path pathTestFormatNotSupported = Path.of("src/test/resources/TestFormatNotSupported.txt");
 
-    @Test
-    void testDifferJson() throws DifferExceptions, IOException {
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
-        assertThat(Differ.generate(pathTestFile1, pathTestFile2)).isEqualTo(expected);
-    }
-
-    @Test
-    void testDifferYaml() throws DifferExceptions, IOException {
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
-        assertThat(Differ.generate(pathTestFile3, pathTestFile4)).isEqualTo(expected);
-    }
+//    @Test
+//    void testDifferJson() throws DifferExceptions, IOException {
+//        String expected = "{\n"
+//                + "  - follow: false\n"
+//                + "    host: hexlet.io\n"
+//                + "  - proxy: 123.234.53.22\n"
+//                + "  - timeout: 50\n"
+//                + "  + timeout: 20\n"
+//                + "  + verbose: true\n"
+//                + "}";
+//        assertThat(Differ.generate(pathTestFile1, pathTestFile2)).isEqualTo(expected);
+//    }
+//
+//    @Test
+//    void testDifferYaml() throws DifferExceptions, IOException {
+//        String expected = "{\n"
+//                + "  - follow: false\n"
+//                + "    host: hexlet.io\n"
+//                + "  - proxy: 123.234.53.22\n"
+//                + "  - timeout: 50\n"
+//                + "  + timeout: 20\n"
+//                + "  + verbose: true\n"
+//                + "}";
+//        assertThat(Differ.generate(pathTestFile3, pathTestFile4)).isEqualTo(expected);
+//    }
 
     @Test
     void testDifferJsonYamlMix() throws DifferExceptions, IOException {
