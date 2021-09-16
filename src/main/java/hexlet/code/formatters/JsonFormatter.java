077b62bd4bd202df8e87b9a630fc76f08c49f204
igor.onuferko <igor.nfrk@gmail.com>
2021-09-16 18:28:47 +0200

    getValueInJsonFormat() refactor4

--- src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -75,28 +75,55 @@ public class JsonFormatter implements FormatterFactory {
             case "Integer":
                 return inputValue.toString();
             case "ArrayList":
-                List<Object> list = (List<Object>) inputValue;
-                StringBuilder arrListSb = new StringBuilder();
-                arrListSb.append("[");
-                for (Object o : list) {
-                    arrListSb.append(getValueInJsonFormat(o)).append(", ");
-                }
-                arrListSb.setLength(arrListSb.length() - 2);
-                arrListSb.append("]");
-                return arrListSb.toString();
+//                List<Object> list = (List<Object>) inputValue;
+//                StringBuilder arrListSb = new StringBuilder();
+//                arrListSb.append("[");
+//                for (Object o : list) {
+//                    arrListSb.append(getValueInJsonFormat(o)).append(", ");
+//                }
+//                arrListSb.setLength(arrListSb.length() - 2);
+//                arrListSb.append("]");
+//                return arrListSb.toString();
+                return getObjectArrayListInJsonFormat(inputValue);
             case "LinkedHashMap":
-                Map<Object, Object> map = (LinkedHashMap<Object, Object>) inputValue;
-                StringBuilder lHMSb = new StringBuilder();
-                lHMSb.append("{\n");
-                for (Map.Entry<Object, Object> e : map.entrySet()) {
-                    lHMSb.append("\t\t\t\t").append(getValueInJsonFormat(e.getKey())).append(": ").
-                            append(getValueInJsonFormat(e.getValue())).append(",\n");
-                }
-                lHMSb.setLength(lHMSb.length() - 2);
-                lHMSb.append("\n\t\t\t}");
-                return lHMSb.toString();
+//                Map<Object, Object> map = (LinkedHashMap<Object, Object>) inputValue;
+//                StringBuilder lHMSb = new StringBuilder();
+//                lHMSb.append("{\n");
+//                for (Map.Entry<Object, Object> e : map.entrySet()) {
+//                    lHMSb.append("\t\t\t\t").append(getValueInJsonFormat(e.getKey())).append(": ").
+//                            append(getValueInJsonFormat(e.getValue())).append(",\n");
+//                }
+//                lHMSb.setLength(lHMSb.length() - 2);
+//                lHMSb.append("\n\t\t\t}");
+//                return lHMSb.toString();
+                return getObjectLinkedHashMapInJsonFormat(inputValue);
             default:
                 return "\"" + inputValue.toString() + "\"";
         }
+    }
+
+    private static String getObjectArrayListInJsonFormat(Object inputValue) {
+        List<Object> list = (List<Object>) inputValue;
+        StringBuilder arrListSb = new StringBuilder();
+        arrListSb.append("[");
+        for (Object o : list) {
+            arrListSb.append(getValueInJsonFormat(o)).append(", ");
+        }
+        arrListSb.setLength(arrListSb.length() - 2);
+        arrListSb.append("]");
+        return arrListSb.toString();
+    }
+
+    private static String getObjectLinkedHashMapInJsonFormat(Object inputValue) {
+        Map<Object, Object> map = (LinkedHashMap<Object, Object>) inputValue;
+        StringBuilder lHMSb = new StringBuilder();
+        lHMSb.append("{\n");
+        for (Map.Entry<Object, Object> e : map.entrySet()) {
+            lHMSb.append("\t\t\t\t").append(getValueInJsonFormat(e.getKey())).append(": ").
+                    append(getValueInJsonFormat(e.getValue())).append(",\n");
+        }
+        lHMSb.setLength(lHMSb.length() - 2);
+        lHMSb.append("\n\t\t\t}");
+        return lHMSb.toString();
     }
 }
