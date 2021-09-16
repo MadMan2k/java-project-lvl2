25c33a88c002f6f9d0af0edb399e2b5103d802b7
igor.onuferko <igor.nfrk@gmail.com>
2021-09-16 23:38:52 +0200

    add YAMLFactory() in ObjectMapper

--- src/main/java/hexlet/code/parserFactory/YamlParser.java
+++ src/main/java/hexlet/code/parserFactory/YamlParser.java
@@ -2,6 +2,7 @@ package hexlet.code.parserFactory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +14,7 @@ public class YamlParser implements ParserFactory {
      */
     @Override
     public Map<String, Object> parse(String inputPath) throws IOException {
-        ObjectMapper ob = new ObjectMapper();
+        ObjectMapper ob = new ObjectMapper(new YAMLFactory());
         return ob.readValue(new File(inputPath),
                 new TypeReference<Map<String, Object>>() { });
     }
