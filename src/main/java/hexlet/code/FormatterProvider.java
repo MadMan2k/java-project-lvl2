cfe2f470667ed95eb7b51f371d26f811eeb0c9d3
igor.onuferko <igor.nfrk@gmail.com>
2021-09-22 13:56:06 +0200

    FormatterProvider.java: if-else => switch-case

--- src/main/java/hexlet/code/FormatterProvider.java
+++ src/main/java/hexlet/code/FormatterProvider.java
@@ -7,14 +7,22 @@ import hexlet.code.formatters.StylishFormatter;
 
 public class FormatterProvider {
     public static Formatter createFormatterByFormat(String format) throws DifferExceptions {
-        if (format.equalsIgnoreCase("stylish")) {
-            return new StylishFormatter();
-        } else if (format.equalsIgnoreCase("plain")) {
-            return new PlainFormatter();
-        }   else if (format.equalsIgnoreCase("json")) {
-            return new JsonFormatter();
-        } else {
-            throw new DifferExceptions(format + " not supported. Only stylish/plain/json output allowed");
-        }
+        return switch (format) {
+            case "stylish" -> new StylishFormatter();
+            case "plain" -> new PlainFormatter();
+            case "json" -> new JsonFormatter();
+            default -> throw new DifferExceptions(format + " not supported. Only stylish/plain/json output allowed");
+        };
+
+
+//        if (format.equalsIgnoreCase("stylish")) {
+//            return new StylishFormatter();
+//        } else if (format.equalsIgnoreCase("plain")) {
+//            return new PlainFormatter();
+//        }   else if (format.equalsIgnoreCase("json")) {
+//            return new JsonFormatter();
+//        } else {
+//            throw new DifferExceptions(format + " not supported. Only stylish/plain/json output allowed");
+//        }
     }
 }
