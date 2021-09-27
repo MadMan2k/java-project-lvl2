1b873f392a684d85e967fbf44a97b8a47810165e
igor.onuferko <igor.nfrk@gmail.com>
2021-09-27 13:54:12 +0200

    checkstyle fix

--- src/main/java/hexlet/code/formatters/JsonFormatterNew.java
+++ src/main/java/hexlet/code/formatters/JsonFormatterNew.java
@@ -4,16 +4,12 @@
 //import com.fasterxml.jackson.core.JsonProcessingException;
 //import com.fasterxml.jackson.databind.ObjectMapper;
 //
-//import java.lang.reflect.Array;
-//import java.util.ArrayList;
-//import java.util.HashMap;
 //import java.util.LinkedHashMap;
-//import java.util.List;
 //import java.util.Map;
 //import java.util.Objects;
 //import java.util.Set;
 //
-//public class JsonFormatter_new implements Formatter {
+//public class JsonFormatterNew implements Formatter {
 //    private static final String OLD_VALUE = "oldValue";
 //    private static final String NEW_VALUE = "newValue";
 //    private static final String STATUS = "status";
@@ -22,14 +18,15 @@
 //     * Make JSON output.
 //     */
 //    @Override
-//    public String format(Map<String, Object> firstMap, Map<String, Object> secondMap, Set<String> keySet) throws JsonProcessingException {
+//    public String format(Map<String, Object> firstMap, Map<String, Object> secondMap,
+//                         Set<String> keySet) throws JsonProcessingException {
 //        int counter = -1;
 //        Map<String, Object[]> mapOfFinal = new LinkedHashMap<>();
 //        Object[] maps = new Object[keySet.size()];
 //        String finalMapStringValue = "jsonDiff";
 //
 //        for (String keyElement : keySet) {
-//            counter ++;
+//            counter++;
 //            Map<String, Object> tempMap = new LinkedHashMap<>();
 //            tempMap.put("field", keyElement);
 //
