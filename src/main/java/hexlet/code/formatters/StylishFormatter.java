50f4dc0002d22537a93d617b4ec0515a03674482
igor.onuferko <igor.nfrk@gmail.com>
2021-09-27 12:17:51 +0200

    StylishFormatter add constants for "+", "-", " ", ":", "\n"

--- src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -5,31 +5,36 @@ import java.util.Objects;
 import java.util.Set;
 
 public class StylishFormatter implements Formatter {
+    private static final String ADDED = "  + ";
+    private static final String REMOVED = "  - ";
+    private static final String UNAFFECTED = "    ";
+    private static final String COLON = ": ";
+    private static final String NEW_LINE = "\n";
     /**
      * Make stylish output.
      */
     @Override
     public String format(Map<String, Object> firstMap, Map<String, Object> secondMap, Set<String> keySet) {
         StringBuilder sb = new StringBuilder();
-        sb.append("{\n");
+        sb.append("{").append(NEW_LINE);
 
         for (String keyElement : keySet) {
             if (!firstMap.containsKey(keyElement) && secondMap.containsKey(keyElement)) {
-                sb.append("  + ").append(keyElement).append(": ").append(secondMap.get(keyElement)).append("\n");
+                sb.append(ADDED).append(keyElement).append(COLON).append(secondMap.get(keyElement)).append(NEW_LINE);
                 continue;
             }
             if (firstMap.containsKey(keyElement) && !secondMap.containsKey(keyElement)) {
-                sb.append("  - ").append(keyElement).append(": ").append(firstMap.get(keyElement)).append("\n");
+                sb.append(REMOVED).append(keyElement).append(COLON).append(firstMap.get(keyElement)).append(NEW_LINE);
                 continue;
             }
             if (Objects.equals(firstMap.get(keyElement), secondMap.get(keyElement))) {
-                sb.append("    ").append(keyElement).append(": ").append(firstMap.get(keyElement)).append("\n");
+                sb.append(UNAFFECTED).append(keyElement).append(COLON).append(firstMap.get(keyElement)).append(NEW_LINE);
                 continue;
             }
             if (firstMap.containsKey(keyElement) && secondMap.containsKey(keyElement)
                     && !Objects.equals(firstMap.get(keyElement), secondMap.get(keyElement))) {
-                sb.append("  - ").append(keyElement).append(": ").append(firstMap.get(keyElement)).append("\n");
-                sb.append("  + ").append(keyElement).append(": ").append(secondMap.get(keyElement)).append("\n");
+                sb.append(REMOVED).append(keyElement).append(COLON).append(firstMap.get(keyElement)).append(NEW_LINE);
+                sb.append(ADDED).append(keyElement).append(COLON).append(secondMap.get(keyElement)).append(NEW_LINE);
             }
         }
         sb.append("}");
